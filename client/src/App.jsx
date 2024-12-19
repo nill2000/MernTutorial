84423481ce8bbc5e4b31ba12097db8092426d4a8
Steve <lysoll8444@gmail.com>
2024-12-19 15:50:27 -0600

    Post from Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
 import './App.css'
+import {useState, useEffect} from "react";
+import Axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+	
+	const [users, setUsers] = useState([]);
+	const [name, setName] = useState("");
+	const [age, setAge] = useState(0);
+	const [username, setUserName] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+	useEffect(() => {
+		Axios.get("http://localhost:3001/getUsers").then((response) => {
+			setUsers(response.data);
+		})
+	}, []);
+
+	const createUser = () => {
+		Axios.post("http://localhost:3001/createUsers", {
+		name, 
+		age, 
+		username
+		}).then((response) => {
+			setUsers([...users, {name, age, username}])
+		});
+	}
+	
+	return (
+		<div>
+			{/* Get data from db and display */}
+			<div className='displayUsers'>
+				{users.map((user) => {
+					return (
+						<div>
+							<h1>User: {user.name}</h1>
+							<h1>age: {user.age}</h1>
+							<h1>username: {user.username}</h1>
+						</div>
+					)
+				})}
+			</div>
+
+				{/* Post Data into db */}
+			<div className='createUsers'>
+				<input onChange={(event) => setName(event.target.value)} type="text" placeholder='name'/>
+				<input onChange={(event) => setAge(event.target.value)} type="number" placeholder='age'/>
+				<input onChange={(event) => setUserName(event.target.value)} type="text" placeholder='username'/>
+				<button onClick={createUser}>Submit User</button>
+			</div>
+		</div>
+	)
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -39,6 +39,6 @@ app.post("/createUsers/", async (req, res) => {
 	}
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
 });
